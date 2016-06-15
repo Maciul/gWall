8046647ab48073e1cf837e1db9f29828c888a6d5
Pawel <Pawel.Maciulewski@gmail.com>
2016-06-14 21:15:56 -0600

    Change css

--- routes/gwall.js
+++ routes/gwall.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('gwall');
+  knex.table('posts').select().then(function (posts){
+    res.render('gwall', {posts: posts});
+  });
 });
 
+router.get('/post', function(req, res){
+  res.render('post');
+});
+
+router.post('/post', function(req, res) {
+  // extract values from form
+    var username = req.body.username;
+    var image = req.body.image;
+    var rant = req.body.rant;
+    knex.table('posts').insert({
+        username: username,
+        image: image,
+        rant: rant
+
+    }).then(function() {
+      console.log('you did it');
+    res.redirect('/gwall');
+
+  }).catch(function (error) {
+    next(error);
+});
+});
 module.exports = router;
